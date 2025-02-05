0a892c9d360b76ea61686f84ae226ed35bd90324
gtouk <gtouk010@uottawa.ca>
2025-02-05 00:38:30 -0500

    Message de commit

--- NKAPBackend/src/config/db.js
+++ NKAPBackend/src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // Create a connection pool
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
 
 
 const db = mysql.createPool(urlDB);
